5d4cfa27a0f1272749f5341cbde30864568848bf
tce-general <tce-general@tce-apt-server>
2008-03-31 10:43:09 +0300

    New nightly compile test script.

--- tce/tools/scripts/run_nightly_compiletest.sh
+++ tce/tools/scripts/run_nightly_compiletest.sh
@@ -9,45 +9,83 @@
 # will fail. The script reinstalls TCE and LLVM/TCE only after the compiletest 
 # to avoid extra recompilations.
 
+# global variables
+export REPO_DIR="${HOME}/repo"
+export BRANCH_DIR="${REPO_DIR}/trunk"
+export INSTALLATION_PATH=$HOME/tce-installation
+export LLVM_BIN_DIR=${HOME}/llvm/bin
+export LLVM_FRONTEND_INSTALL_DIR=${HOME}/llvm-frontend
+
+export PATH="$INSTALLATION_PATH/bin:${LLVM_BIN_DIR}:${INSTALLATION_PATH}/bin:${LLVM_FRONTEND_INSTALL_DIR}/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
+export LD_LIBRARY_PATH=$INSTALLATION_PATH/lib
+
 function eexit {
     echo $1 >&2
     exit 1
 }
 
-export INSTALLATION_PATH=$HOME/tce-installation
-export PATH="$INSTALLATION_PATH/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
-export LD_LIBRARY_PATH=$INSTALLATION_PATH/lib
+# $1: full, do full rebuild
+function install_llvm-frontend {
+    SOURCE_DIR=${BRANCH_DIR}/llvm-frontend
+    BUILD_DIR=${BRANCH_DIR}/llvm-frontend/build
 
-cd ~/repo
-cd trunk
-echo "Fetching updates from bzr..."
-bzr up
+    export MAKEFLAGS=-j1
+    export CXX=g++
+    export CXXFLAGS="-02"
+    export CFLAGS="-O2"
+    export CC=gcc
 
-# Recompile and reinstall LLVM/TCE.
+    if [ "${1}x" == "fullx" ]; then
+        cd ${SOURCE_DIR} || return 1
+        autoreconf || return 1
 
-cd ~/repo/trunk/llvm-frontend
-autoreconf > /dev/null 2>&1
-mkdir -p build && cd build
+        # remove build dir
+        rm -rf ${BUILD_DIR}
+        mkdir -p ${BUILD_DIR}
+        cd ${BUILD_DIR} || return 1
+        ${SOURCE_DIR}/configure --prefix=${LLVM_FRONTEND_INSTALL_DIR} || return 1
+    else
+        cd ${BUILD_DIR} || return 1
+    fi
 
-# Only single word compilation commands are tolerated by the LLVM build scripts.
-export CXX=g++
-export CXXFLAGS="-O1 -g"
-export CC=gcc
-TARGET=$(tce-config --prefix)
+    make -s || return 1
+    rm -rf ${LLVM_FRONTEND_INSTALL_DIR}
+    make install || return 1
+}
 
-echo "Installing LLVM/TCE to $TARGET..."
-../configure --prefix=$TARGET >/dev/null 2>&1 || eexit "Configuration of LLVM/TCE failed!" 
-make -s >/dev/null 2>&1 || eexit "Compilation of LLVM/TCE failed!" 
-make install >/dev/null 2>&1 || eexit "Installation of LLVM/TCE failed!"
+function start_compiletest {
+    cd "${BRANCH_DIR}/tce"
+    autoreconf > /dev/null 2>&1
 
-cd ~/repo/trunk/tce
-autoreconf > /dev/null 2>&1
+    export ERROR_MAIL=yes
+    export ERROR_MAIL_ADDRESS=tce-logs@cs.tut.fi
+    export CXX="ccache g++"
+    export CC="ccache gcc"
+    export CXXFLAGS="-O3 -Wall -pedantic -Wno-long-long -g -Wno-variadic-macros"
+    export TCE_CONFIGURE_SWITCHES="--prefix=$INSTALLATION_PATH --disable-python"
 
-export ERROR_MAIL=yes
-export ERROR_MAIL_ADDRESS=tce-logs@cs.tut.fi
-export CXXFLAGS="-O3 -Wall -pedantic -Wno-long-long -g -Wno-variadic-macros"
-export TCE_CONFIGURE_SWITCHES="--prefix=$INSTALLATION_PATH --disable-python"
+    tools/scripts/compiletest.sh $@
 
-tools/scripts/compiletest.sh $*
+    tcecc --clear-plugin-cache
+}
 
-tcecc --clear-plugin-cache
+function update_repo {
+    cd ${BRANCH_DIR}
+    bzr up || return 1
+}
+
+##############################################################
+
+if [ ! -e "${LLVM_BIN_DIR}" ]; then
+    eexit "LLVM bin directory: ${LLVM_BIN_DIR}, was not found."
+fi
+
+# first update repository
+update_repo &>/dev/null || eexit "Updating bzr repository failed."
+
+# now compile and install tce llvm-frontend from trunk branch
+# llvm is expected to be intalled
+install_llvm-frontend &>/dev/null || eexit "Compiling/installing llvm-frontend failed."
+
+# call compiletest.sh
+start_compiletest $@
