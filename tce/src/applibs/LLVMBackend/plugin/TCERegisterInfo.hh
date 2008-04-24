0c9fa95e28193ff8e221bcdd4bb68a83062340c3
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2008-04-24 13:16:53 +0300

    Fixed couple of issues with Heikki's if-conversion stuff, now asserts with
    lt-llvm-tce: 
    /home/visit0r/trunk/tce/src/applibs/LLVMBackend/plugin/TCEInstrInfo.cc:169: virtual 
    void llvm::TCEInstrInfo::storeRegToStackSlot(llvm::MachineBasicBlock&, 
    llvm::ilist_iterator<llvm::MachineInstr>, unsigned int, bool, int, const 
    llvm::TargetRegisterClass*) const: Assertion `0 && "Can't store this register to 
    stack slot"' failed.
    Aborted (core dumped)

--- tce/src/applibs/LLVMBackend/plugin/TCERegisterInfo.hh
+++ tce/src/applibs/LLVMBackend/plugin/TCERegisterInfo.hh
@@ -57,6 +57,23 @@ namespace llvm {
 
         int getDwarfRegNum(unsigned regNum, bool isEH) const;
     
+        void storeRegToStackSlot(
+            MachineBasicBlock& mbb,
+            MachineBasicBlock::iterator mbbi,
+            unsigned srcReg, int fi,
+            const TargetRegisterClass* rc) const;
+
+        void loadRegFromStackSlot(
+            MachineBasicBlock& mbb,
+            MachineBasicBlock::iterator mbbi,
+            unsigned destReg, int fi,
+            const TargetRegisterClass* rc) const;
+
+        void copyRegToReg(
+            MachineBasicBlock& mbb,
+            MachineBasicBlock::iterator mbbi,
+            unsigned destReg, unsigned srcReg,
+            const TargetRegisterClass* rc) const;
 
     private:
         const TargetInstrInfo& tii_;
