18f5ab22c5a7257781534e5bdbfa34fe42adc4ad
Pekka Jääskeläinen <visit0r@aslanov>
2008-08-14 20:48:27 +0300

    Reproduces #44 without using printf(). Data corruption seems to happen when using a tceop macro (in this case the MUL operation) twice in a row.

--- testsuite/systemtest/bintools/Compiler/RegressionTests/data/printf_broken.c
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/data/printf_broken.c
@@ -8,6 +8,7 @@ unsigned int chroma_in2_32b;
 
 unsigned int array[3]={0x9a8aba52, 0x99eb4a0c, 0xac9f4e29};
 
+volatile unsigned char temp1, temp2, temp3;
 
 int main() {
 
@@ -16,10 +17,11 @@ int main() {
 	unsigned int chroma_in1_32b;
 	unsigned int chroma_in2_32b;
 
-	unsigned char A0_Cb, A1_Cb, B0_Cb, B1_Cb, C0_Cb, C1_Cb;
-	unsigned char A0_Cr, A1_Cr, B0_Cr, B1_Cr, C0_Cr, C1_Cr;	
 	unsigned char P0_Cb, Q0_Cb;
 	unsigned char P0_Cr, Q0_Cr;
+    unsigned char A0_Cb, A1_Cb, B0_Cb, B1_Cb, C0_Cb, C1_Cb;
+    unsigned char A0_Cr, A1_Cr, B0_Cr, B1_Cr, C0_Cr, C1_Cr;	
+
 
 	chroma_in0_32b = array[0];
 	chroma_in1_32b = array[1];
@@ -32,20 +34,18 @@ int main() {
 	C1_Cb = (chroma_in2_32b&0xFF000000)>>24;
 	C1_Cr = (chroma_in2_32b&0x00FF0000)>>16;
 
-    /* If this line is added, bug is reproduced: */
-	/* printf("\n B1_Cb: %x, B1_Cr: %x", B1_Cb, B1_Cr); */
+    /* Remove one or both of the following and it starts working again: */
+	_TCE_MUL(B1_Cr, B1_Cb, P0_Cb); 
+	_TCE_MUL(B1_Cr, B1_Cb, Q0_Cb);  
 
+    temp1 = B1_Cb;
+    temp2 = B1_Cr; 
 
-	_TCE_MUL(B1_Cr, B1_Cb, P0_Cb);		
-	_TCE_MUL(B1_Cr, B1_Cb, Q0_Cb);
-
-	printf("\n A1_Cb: %x, A1_Cr: %x", A1_Cb, A1_Cr);
-	printf("\n B1_Cb: %x, B1_Cr: %x", B1_Cb, B1_Cr); /* Prints: "B1_Cb: 0, B1_Cr: 0" */
-	printf("\n C1_Cb: %x, C1_Cr: %x", C1_Cb, C1_Cr);
-	
-
+/* If this is enabled, it starts working. So probably a DCE issue of some kind.
+  
+    temp3 = P0_Cb*Q0_Cb;
+*/
 	return(0);
-
 }
 
 
