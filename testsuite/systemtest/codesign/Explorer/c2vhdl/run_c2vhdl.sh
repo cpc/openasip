e8198ec74cf9b8bf9acce801d41c396660dbca0a
Esa Määttä <esa.maatta@tut.fi>
2008-10-23 11:27:17 +0300

    Updated c2vhdl test to use c2vhdl script.

--- testsuite/systemtest/codesign/Explorer/c2vhdl/run_c2vhdl.sh
+++ testsuite/systemtest/codesign/Explorer/c2vhdl/run_c2vhdl.sh
@@ -1,45 +1,13 @@
 #!/bin/bash
 TCE_ROOT="../../../../../tce"
 
-EXPLORE="${TCE_ROOT}/src/codesign/Explorer/explore"
-TCECC="${TCE_ROOT}/src/bintools/Compiler/tcecc"
-CREATEBEM="${TCE_ROOT}/src/bintools/BEMGenerator/createbem"
-GENERATEPROCESSOR="${TCE_ROOT}/src/procgen/ProGe/generateprocessor"
-GENERATEBITS="${TCE_ROOT}/src/bintools/PIG/generatebits"
-
-ADF="${TCE_ROOT}/data/mach/minimal.adf"
-
-OUTPUT="/dev/null"
-__DEBUG="false"
-if [ "${__DEBUG}x" == "truex" ]; then
-    OUTPUT="debug.log"
-fi
-
-{
-$TCECC data/application1/complex_multiply.c -O2 -o data/program.bc
-echo "running: $EXPLORE -a $ADF -d data/application1 ExpRes.dsdb" 
-$EXPLORE -a $ADF -d data ExpRes.dsdb 
-
-NEW_CONF="$($EXPLORE -e SimpleICOptimizer -s 1 ExpRes.dsdb \
-| tail -n1 | grep -x '[[:space:]][0-9][0-9]*' | tr -d '[[:space:]]')"
-echo "New config created: ${NEW_CONF}" 
-
-NEW_CONF="$($EXPLORE -e ImplementationSelector -s $NEW_CONF ExpRes.dsdb \
-| tail -n1 | grep -x '[[:space:]][0-9][0-9]*' | tr -d '[[:space:]]')"
-echo "New config created: ${NEW_CONF}" 
-
-$EXPLORE -w ${NEW_CONF} ExpRes.dsdb
-$TCECC -a ${NEW_CONF}.adf -o complex_multiply.scheduled.tpef data/program.bc 
-$CREATEBEM ${NEW_CONF}.adf 
-$GENERATEPROCESSOR -b ${NEW_CONF}.bem -i ${NEW_CONF}.idf ${NEW_CONF}.adf 
-$GENERATEBITS -b ${NEW_CONF}.bem -d -p complex_multiply.scheduled.tpef ${NEW_CONF}.adf 
-} 1>${OUTPUT} 2>&1
+${TCE_ROOT}/scripts/c2vhdl data/application1/complex_multiply.c || { echo "c2vhdl script failed."; exit 1; }
 
 # check that image files were created
-if [ ! -e "complex_multiply.scheduled_data.img" ]; then
+if [ ! -e "complex_multiply_data.img" ]; then
     echo "Error: No data image generated."
 fi
 
-if [ ! -e "complex_multiply.scheduled.img" ]; then
+if [ ! -e "complex_multiply.img" ]; then
     echo "Error: No image generated."
 fi
