98732ced03302bb699285ec3f3a41f8c295e3725
Esa Määttä <esa.maatta@tut.fi>
2008-10-23 11:48:45 +0300

    c2vhdl scripts expects that some programs are found from PATH.

--- testsuite/systemtest/codesign/Explorer/c2vhdl/run_c2vhdl.sh
+++ testsuite/systemtest/codesign/Explorer/c2vhdl/run_c2vhdl.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 TCE_ROOT="../../../../../tce"
 
+function addToVarEnd {
+    export $1="${!1}${2}"
+}
+
+function addToPath {
+    if ! $(\grep -qE ":?${1}(:|$)" <(echo "${PATH}")); then
+        addToVarEnd PATH ":$1" 
+    fi  
+}
+
+# program paths
+addToPath "${TCE_ROOT}/src/codesign/Explorer"
+addToPath "${TCE_ROOT}/src/bintools/Compiler"
+addToPath "${TCE_ROOT}/src/bintools/BEMGenerator"
+addToPath "${TCE_ROOT}/src/procgen/ProGe"
+addToPath "${TCE_ROOT}/src/bintools/PIG"
+
 ${TCE_ROOT}/scripts/c2vhdl data/application1/complex_multiply.c || { echo "c2vhdl script failed."; exit 1; }
 
 # check that image files were created
