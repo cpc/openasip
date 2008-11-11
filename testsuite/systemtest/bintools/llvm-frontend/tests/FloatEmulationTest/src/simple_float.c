43315fcd5ac887c2c2e61614a571ea9fb2ddb5ec
Mikael Lepistö <elhigu@mr-lenovo>
2008-11-11 10:30:14 +0200

    a bit more helpful test result.

--- testsuite/systemtest/bintools/llvm-frontend/tests/FloatEmulationTest/src/simple_float.c
+++ testsuite/systemtest/bintools/llvm-frontend/tests/FloatEmulationTest/src/simple_float.c
@@ -4,10 +4,11 @@
 volatile static unsigned a = 4100000000u;
 volatile static int b = 41000;
 
+volatile float aFloat, bFloat;
+volatile float resMul, resDiv, resMod, resAdd, resSub, resNeg;
+
 int main(int argc,char **argv,char **envp) { 
 
-    float aFloat, bFloat;
-    float resMul, resDiv, resMod, resAdd, resSub, resNeg;
     
     aFloat = a;
     bFloat = b;
@@ -20,13 +21,6 @@ int main(int argc,char **argv,char **envp) {
     resDiv = aFloat / bFloat;
 //    resMod = aFloat % bFloat;
     
-
-    printf("%x+%x=%x\n", *((int*)&aFloat), *((int*)&bFloat), *((int*)&resAdd));  
-    printf("%x-%x=%x\n", *((int*)&aFloat), *((int*)&bFloat), *((int*)&resSub));  
-    printf("%x*%x=%x\n", *((int*)&aFloat), *((int*)&bFloat), *((int*)&resMul));  
-    printf("%x/%x=%x\n", *((int*)&aFloat), *((int*)&bFloat), *((int*)&resDiv));  
-    printf("-%x=%x\n", *((int*)&aFloat), *((int*)&resNeg));  
-
     unsigned srcVal = a;
     float tempFloat;
     unsigned dstVal1, dstVal2;
@@ -39,11 +33,11 @@ int main(int argc,char **argv,char **envp) {
     printf("%x to fp to uint %x\n", srcVal, dstVal1);  
     printf("%x to fp to -fp to uint %x\n", srcVal, dstVal2);  
 
-    printf("%f+%f=%f\n", aFloat, bFloat, resAdd);
-    printf("%f-%f=%f\n", aFloat, bFloat, resSub);
-    printf("%f*%f=%f\n", aFloat, bFloat, resMul);
-    printf("%f/%f=%f\n", aFloat, bFloat, resDiv);
-    printf("-%f=%f\n", aFloat, resNeg);
+    printf("%f+%f=%f in hex format: %x+%x=%x\n", aFloat, bFloat, resAdd, *((int*)&aFloat), *((int*)&bFloat), *((int*)&resAdd));  
+    printf("%f-%f=%f in hex format: %x-%x=%x\n", aFloat, bFloat, resSub, *((int*)&aFloat), *((int*)&bFloat), *((int*)&resSub));  
+    printf("%f*%f=%f in hex format: %x*%x=%x\n", aFloat, bFloat, resMul, *((int*)&aFloat), *((int*)&bFloat), *((int*)&resMul));  
+    printf("%f/%f=%f in hex format: %x/%x=%x\n", aFloat, bFloat, resDiv, *((int*)&aFloat), *((int*)&bFloat), *((int*)&resDiv));  
+    printf("-%f=%f in hex format: -%x=%x\n", aFloat, resNeg, *((int*)&aFloat), *((int*)&resNeg));
 
     if (resDiv > resMul) return 1;
     if (resMod >= resAdd) return 2;
