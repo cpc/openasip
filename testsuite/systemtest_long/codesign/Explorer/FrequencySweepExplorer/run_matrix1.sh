21b404609f1f95c304da16122358afa53d7ea9f1
Esa Määttä <esa.maatta@tut.fi>
2008-11-17 14:37:07 +0200

    Updated Frequency Sweeper explorer plugin systemtest.

--- testsuite/systemtest_long/codesign/Explorer/FrequencySweepExplorer/run_matrix1.sh
+++ testsuite/systemtest_long/codesign/Explorer/FrequencySweepExplorer/run_matrix1.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-EXPLORE_BIN="../../../../../tce/src/codesign/Explorer/explore"
+TCE_ROOT="../../../../../tce"
+EXPLORE_BIN="${TCE_ROOT}/src/codesign/Explorer/explore"
 
-PLUGIN="-e FrequencySweepExplorer"
-TPEF="-d ../../../../../tce/systemtest/bintools/Scheduler/tests/DSPstone/fixed_point/matrix/matrix1"
+PLUGIN="FrequencySweepExplorer"
+APPDIR="../../../../../testsuite/systemtest/bintools/Scheduler/tests/DSPstone/fixed_point/matrix/matrix1"
 DSDB="testi.dsdb"
+ADF="data/minimal_6x32.adf"
 
-${EXPLORE_BIN} -a ../../../../../tce/data/mach/minimal.adf ${DSDB}
-${EXPLORE_BIN} ${PLUGIN} ${TPEF} \
--s 1 -u verbose=0 -u start_freq_mhz=100 -u end_freq_mhz=150 -u step_freq_mhz=50 \
-${DSDB}
 
-#RESULTS=($(${EXPLORE_BIN} ${PLUGIN} ${TPEF} \
-#-s 1 -u verbose=0 -u start_freq_mhz=100 -u end_freq_mhz=150 -u step_freq_mhz=50 \
-#${DSDB} | grep -Ex '^[[:space:]]{1,}[0-9]{1,}' | grep -Eo '[0-9]{1,}' | xargs))
+${EXPLORE_BIN} -a ${ADF} -d ${APPDIR} ${DSDB} 1>/dev/null
 
-exit
+RESULTS=($(${EXPLORE_BIN} -e ${PLUGIN} \
+-s 1 -u start_freq_mhz=100 -u end_freq_mhz=150 -u step_freq_mhz=50 \
+${DSDB}  | grep -Ex '^[[:space:]]{1,}[0-9]{1,}' | grep -Eo '[0-9]{1,}' | xargs))
 
-[ "${#RESULTS[@]}" -lt 2 ] && (echo "Not enough configures created."; exit 1)
+[ "${#RESULTS[@]}" -lt 4 ] && (echo "Not enough configurations created (${#RESULTS[@]}/4)"; exit 1)
 
 for result in ${RESULTS[@]}; do
     ${EXPLORE_BIN} -w "$result" ${DSDB} 1>/dev/null
