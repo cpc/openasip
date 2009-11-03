234b00623de4fd5ec9986a9295b481fa28532e0f
Mikael Lepistö <elhigu@mr-lenovo>
2009-11-03 13:21:58 +0200

    script for finding buildbot tested llvm-svn revision

---
+++ tce/tools/scripts/find_good_llvm_svn_revision.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Finds last llvm svn version which has passed the most buildbot tests.
+
+Checks last 100 revisions and.
+
+author Mikael Lepistö 2009
+"""
+
+import urllib2
+import BeautifulSoup
+
+soup = BeautifulSoup.BeautifulSoup(
+    urllib2.urlopen('http://google1.osuosl.org:8011/grid?width=100').read())
+
+revcount = len(soup.find('tr').findAll('td'))
+
+oks_per_column = [[0,0]] + [[0, int(i.string)]
+                            for i in soup.findAll('td', 'sourcestamp')]
+
+for i in soup.findAll('tr'):
+	col = 0
+	for row in i.findAll('td'):
+		link = row.find('a')
+		if link and link.string == u'OK':
+			oks_per_column[col][0] += 1
+		col += 1
+
+print max(oks_per_column)[1]
