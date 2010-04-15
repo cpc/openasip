7ea1531c477724f13d7f2f9ecf0f2a1fcb93c1fb
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2010-04-15 15:18:23 +0300

    Added topresults to CHStone tests. Disabled GSM verification (see release-bug #129).

--- testsuite/systemtest/bintools/Scheduler/tests/CHStone/gsm/src/gsm.c
+++ testsuite/systemtest/bintools/Scheduler/tests/CHStone/gsm/src/gsm.c
@@ -109,7 +109,9 @@ main ()
           
        if(main_result != 0)
        {
-             puts("FAIL!\n");
+           puts("FAIL!\n");
+       } else {
+           puts("OK\n");
        }
       return main_result;
     }
