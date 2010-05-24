0717de8342db267273a81992c7185e5c1cdb4518
Heikki Kultala <hkultala@tiberias>
2010-05-24 14:06:01 +0300

    sqrt test no logner uses printf.
    Makes the test run(compile) faster.

--- testsuite/systemtest/bintools/Compiler/RegressionTests/data/test_sqrt.c
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/data/test_sqrt.c
@@ -7,7 +7,17 @@ volatile float f = 0.54f;
 int main() {
     d = 4.00f;
     f = 4.00f;
-    printf("square root of %f is %f\n", d, sqrt(d));
-    printf("square root of %f is %f\n", f, sqrtf(f));
+    d = sqrt(d);
+    if (d == 2.00) {
+	puts("OK");
+    } else {
+	puts("FAIL");
+    }
+    f = sqrtf(f);
+    if (f == 2.00f) {
+	puts("OK");
+    } else {
+	puts("FAIL");
+    }
     return 0;
 }
