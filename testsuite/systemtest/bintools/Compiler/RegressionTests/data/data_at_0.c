69df42abc5f019e42ea95e0e96e423500d79b7bc
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2010-05-25 14:44:56 +0300

    llvm-svn converts the compile time null dereference to a call void @llvm.trap() unreachable which gets converted to abort() which is not linked in at that point. Changing the 0 to be read from a volatile variable avoided this smart optimization (should fix this later in case this occurs often).

--- testsuite/systemtest/bintools/Compiler/RegressionTests/data/data_at_0.c
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/data/data_at_0.c
@@ -1,9 +1,9 @@
 #include <assert.h>
 
-volatile char a=1,b=2,c=3;
+volatile char a=1,b=2,c=3, addr=0;
 
 int main() {
-    volatile int value = *((int*)(0));
+    volatile int value = *((int*)(addr));
     assert(value == 0);
     return value;
 }
