7ca0ea977db709233421c7f3f717d04d3b58f772
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2010-11-03 15:55:18 +0200

    Systemtest for the -g file name/lineno info display in tcedisasm.

---
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/data/lineno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+__attribute__((noinline))
+int test_function(int* x, int* y)
+{
+   (*x)++;
+   (*y)--;
+   return *x;
+}
+
+volatile int z = 0;
+__attribute__((noinline))
+int main() {
+    int x = 1 , y = 2;
+    z = x + y;
+    x = test_function(z, y);
+    y = test_function(z, y);
+    return z + y;
+}
