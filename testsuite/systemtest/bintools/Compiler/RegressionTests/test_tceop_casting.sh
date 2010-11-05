e5e4840957fa5eebe283d693b437eca545cf5255
Otto Esko <otski@ruiskerpele>
2010-11-05 16:58:35 +0200

    Fix for tce op casting test

--- testsuite/systemtest/bintools/Compiler/RegressionTests/test_tceop_casting.sh
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/test_tceop_casting.sh
@@ -9,7 +9,8 @@ adf=../../../../../tce/data/mach/minimal.adf
 $tcecc -O0 -a $adf -o $tpef \
 -k main,result,chroma_in0_32b data/tceop_casting.c
 
-$tceopgen 2>&1 | egrep "STW|MUL|ADD|DIVU|XOR" | egrep -v "_MULT"
+$tceopgen 2>&1 | egrep "STW|MUL|ADD|DIVU|XOR" | egrep -v "_MULT" \
+| egrep -v "_AVALON"
 
 echo $($ttasim -p $tpef -a $adf -e "run; puts [x /u w result]; quit;" --no-debugmode)
 
