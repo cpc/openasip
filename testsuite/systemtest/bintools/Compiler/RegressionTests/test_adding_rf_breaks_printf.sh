a8a11c0d86212198ac6f83e657c9241eba77b6ba
Vladimir Guzma <vladimir.guzma@tut.fi>
2011-01-29 11:34:54 +0200

    No debugmode, so ttasim exists if there is problem instead of waiting on command line.

--- testsuite/systemtest/bintools/Compiler/RegressionTests/test_adding_rf_breaks_printf.sh
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/test_adding_rf_breaks_printf.sh
@@ -9,8 +9,8 @@ tpef=/tmp/fooo234sdwe342klwemwet.124.tpef
 
 function test_machine {
   $tcecc -O0 -a $1 -o $tpef -k main data/printf_broken.c
-  $ttasim -a $1 -p $tpef -e "run; puts [x /u b temp1]; puts [x /u b temp2]; quit;"
-  rm -f $tpef
+  $ttasim --no-debugmode -a $1 -p $tpef -e "run; puts [x /u b temp1]; puts [x /u b temp2]; quit;"
+  rm -f $tpef 
 }
 
 test_machine data/printf_machine_works.adf
