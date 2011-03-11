c3eaf9fdbfc1f6387f2108fe61bbc920ecc17dbb
R0bot <>
2011-03-11 10:05:26 +0200

    Fixed for LLVM 2.9. It produced more pareto results.

--- testsuite/systemtest_long/codesign/Explorer/ConnectionSweeper/run_connectionsweeper.sh
+++ testsuite/systemtest_long/codesign/Explorer/ConnectionSweeper/run_connectionsweeper.sh
@@ -7,5 +7,16 @@ ADF_PATH="./data/minimal2bus.adf"
 "${EXPLORE_BIN}" -a ${ADF_PATH} testi.dsdb 1>/dev/null
 "${EXPLORE_BIN}" -d ./data/ testi.dsdb 1>/dev/null
 ${EXPLORE_BIN} -e ConnectionSweeper -s 1 -u cc_worsening_threshold=10 testi.dsdb | grep -v "No fitting"
-${EXPLORE_BIN} --pareto_set C testi.dsdb | wc -l
+ROWS="$(${EXPLORE_BIN} --pareto_set C testi.dsdb | wc -l)"
+
+# With LLVM 2.8 one gets 7, with LLVM 2.9 one gets 11 rows.
+MIN_ROWS=6
+
+if [ "${ROWS}" -lt "${MIN_ROWS}" ]; then
+   echo "Not enough rows in the pareto set result!"
+else
+   echo "Looking fine. Got some archs!"
+fi
+
+
 
