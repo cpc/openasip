2daedd7151e2a68ca09dc64da5afcab30b41597f
hkultala@cs.tut.fi <>
2011-03-25 14:19:21 +0200

    whoops, one debug change was left on

--- tce/src/applibs/Simulator/SimulatorFrontend.cc
+++ tce/src/applibs/Simulator/SimulatorFrontend.cc
@@ -120,7 +120,7 @@ SimulatorFrontend::SimulatorFrontend(bool useCompiledSimulation) :
     printNextInstruction_(true), printSimulationTimeStatistics_(false),
     staticCompilation_(true), traceFileNameSetByUser_(false), outputStream_(0),
     memoryAccessTracking_(false), eventHandler_(NULL), lastRunCycleCount_(0), 
-    lastRunTime_(0.0), simulationTimeout_(0), leaveCompiledDirty_(true) {
+    lastRunTime_(0.0), simulationTimeout_(0), leaveCompiledDirty_(false) {
 
     if (compiledSimulation_) {
         setFUResourceConflictDetection(false); // disabled by default
