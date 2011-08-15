5769edf7f72fabd1dc414beba7e0a02516604952
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2011-08-15 10:11:14 +0300

    MIT license, updated to do.

--- tce/tools/scripts/systemtest.py
+++ tce/tools/scripts/systemtest.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 # -!- coding: utf-8 -!-
 """
+    Copyright (c) 2011 Tampere University of Technology.
+
+    This file is part of TTA-Based Codesign Environment (TCE).
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
+"""
 Integration/systemtest script for the TCE tools.
 
 To do:
-* Support executing run_*.sh files directly. 
-  * Read metadata from the script's comments. 
+* Support executing {run_,test_,run-,test-}.{py,sh} files directly. 
+  * Read metadata (description, maybe expected output?) from the 
+    script's comments. 
   * Setup PATH so one does not need relative paths to the src-tree 
-    binaries, thus enable testing of build tree builds and even
-    installations.
+    binaries, thus enable testing of build tree builds and even testing
+    TCE installations.
 
-@author Pekka Jääskeläinen 2011
+@author 2011 Pekka Jääskeläinen 
 """
 import os
 import tempfile
