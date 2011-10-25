fc9d2aa04bcca5005c24a411b6a371714b654f75
Heikki Kultala <hkultala@cs.tut.fi>
2011-10-25 17:15:05 +0300

    set ld library path on ttasim/systemc integration test. test might fail if not set.

--- testsuite/systemtest_long/codesign/ttasim/test_systemc.sh
+++ testsuite/systemtest_long/codesign/ttasim/test_systemc.sh
@@ -4,4 +4,5 @@
 # to the test suite, but otherwise around it's fine.
 cd ../../../../tce/doc/man/TCE/systemc_example
 make -s
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(llvm-config --libdir)
 ./simulator 2>&1 | grep recv
