b6c9b01d1a430f5e5a5b560b31cf53a55047cd5d
Vladimir Guzma <vladimir.guzma@tut.fi>
2012-02-21 14:32:34 +0200

    Systemtest fix.

--- testsuite/systemtest/codesign/Explorer/ADFCombiner/run_adfcombiner.sh
+++ testsuite/systemtest/codesign/Explorer/ADFCombiner/run_adfcombiner.sh
@@ -8,22 +8,22 @@ ADF_PATH="${MINIMAL_ADF_PATH}"
 NODE_COUNT=6
 ${EXPLORE_BIN} -e ADFCombiner -u node=${MINIMAL_ADF_PATH} -u extra=${MINIMAL_ADF_PATH} -u node_count=${NODE_COUNT} -u build_idf=true  test.dsdb 1>/dev/null
 
-"${EXPLORE_BIN}" -w 3 test.dsdb 1>/dev/null
+"${EXPLORE_BIN}" -w 2 test.dsdb 1>/dev/null
 
-CONNECTING_BUSES="$(grep '<bus name.*>' 3.adf |grep "connect"| wc -l)"
+CONNECTING_BUSES="$(grep '<bus name.*>' 2.adf |grep "connect"| wc -l)"
 # NODE_COUNT buses for each node internal connections
 # NODE_COUNT + 1 buses for each register file and and neighbour register files, including extra
 ((REQUIRED_BUSES=(NODE_COUNT+1)*2 + NODE_COUNT))
 if [ ! "${CONNECTING_BUSES}" -eq "${REQUIRED_BUSES}" ]; then
     echo "Added wrong number of connecting buses (${CONNECTING_BUSES} != ${REQUIRED_BUES})."
 fi
-CONNECTING_SOCKET_R="$(grep '<socket name.*>' 3.adf |grep "connect_r"| wc -l)"
-CONNECTING_SOCKET_W="$(grep '<socket name.*>' 3.adf |grep "connect_w"| wc -l)"
+CONNECTING_SOCKET_R="$(grep '<socket name.*>' 2.adf |grep "connect_r"| wc -l)"
+CONNECTING_SOCKET_W="$(grep '<socket name.*>' 2.adf |grep "connect_w"| wc -l)"
 if [ ! "${CONNECTING_SOCKET_R}" -eq "${CONNECTING_SOCKET_W}" ]; then
     echo "Added wrong number of connecting sockets between register files (${CONNECTING_SOCKET_R} != ${CONNECTING_SOCKET_W})."
 fi
 IDF=`ls *.idf`
-if [ ! "${IDF}" = "3.idf" ]; then
+if [ ! "${IDF}" = "2.idf" ]; then
     echo "Creation of IDF file failed. ${IDF}"
 fi
-rm -rf 3.adf 3.idf
+rm -rf 2.adf 2.idf
