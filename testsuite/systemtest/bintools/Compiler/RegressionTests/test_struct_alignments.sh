a5aedeed47d1d72068bb9965c9a330747f8c3ee0
Vladimir Guzma <vladimir.guzma@tut.fi>
2012-02-29 14:54:40 +0200

    If compiling fails, do not get stuck in the simulator command line.

--- testsuite/systemtest/bintools/Compiler/RegressionTests/test_struct_alignments.sh
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/test_struct_alignments.sh
@@ -5,6 +5,6 @@ adf=data/printf_machine_works.adf
 ttasim=../../../../../tce/src/codesign/ttasim/ttasim
 
 $tcecc -O0 -a $adf -o $tpef data/struct_alignments.c
-$ttasim -p $tpef -a $adf -e "run; quit;"
+$ttasim --no-debugmode -p $tpef -a $adf -e "run; quit;"
 
 rm -f $tpef
