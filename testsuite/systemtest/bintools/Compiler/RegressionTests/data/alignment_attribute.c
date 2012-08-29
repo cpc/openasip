ed56c648ad0f30564b44600fd8f8050fcb4e4d6d
hkultala@cs.tut.fi <>
2012-08-29 13:44:09 +0300

    alignment attribute test also tests uninitialized table. seems to work.

--- testsuite/systemtest/bintools/Compiler/RegressionTests/data/alignment_attribute.c
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/data/alignment_attribute.c
@@ -1,14 +1,22 @@
 #include <lwpr.h>
 
 volatile int a __attribute__((aligned(8))) = 13;
+volatile int b[2] __attribute__((aligned(64)));
 
 int main() {
 
     int addr = (int)&a;
+    int addr2 = (int)&b;
     lwpr_print_int(addr);_TCE_STDOUT('\n');
+    lwpr_print_int(addr2);_TCE_STDOUT('\n');
     if (addr%8 != 0)
         lwpr_print_str("Variable a is misaligned!\n");
     else
         lwpr_print_str("Variable a is correctly aligned!\n");
+    if (addr2%64 != 0)
+        lwpr_print_str("Variable b is misaligned!\n");
+    else
+        lwpr_print_str("Variable b is correctly aligned!\n");
+
     return 0;
 }
