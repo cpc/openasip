15f500f259e836fce92bf12b709efa00818509c9
Otto Esko <eskoo@tarharastas>
2012-12-17 13:28:04 +0200

    llvm-tce-ld path is passed as a parameter to DENBench script

--- testsuite/systemtest_longlong/bintools/Compiler/denbench_verification.sh
+++ testsuite/systemtest_longlong/bintools/Compiler/denbench_verification.sh
@@ -4,6 +4,7 @@ tce=$PWD/../../../../tce/
 machine=${tce}/scheduler/testbench/ADF/ti64x_subset.adf
 ttasim=${tce}/src/codesign/ttasim/ttasim
 tcecc=${tce}/src/bintools/Compiler/tcecc
+tce_ld=${tce}/src/ext/llvm-ld/tce-llvm-ld
 optlevel=-O0
 # this was default behavior
 scheduler_flags=-O2
@@ -12,5 +13,5 @@ verbose=""
 
 cd DENBench
 rm -fr consumer/llvm-tce-systemtest 
-./run_systemtest.sh -c $tcecc -t $ttasim -a $machine $optlevel -s "$scheduler_flags" $verbose | \
+./run_systemtest.sh -c $tcecc -t $ttasim -l $tce_ld -a $machine $optlevel -s "$scheduler_flags" $verbose | \
 grep -v mp4decode
--- testsuite/systemtest_longlong/bintools/Compiler/denbench_verification_verbose.sh
+++ testsuite/systemtest_longlong/bintools/Compiler/denbench_verification_verbose.sh
@@ -4,6 +4,7 @@ tce=$PWD/../../../../tce/
 machine=${tce}/scheduler/testbench/ADF/ti64x_subset.adf
 ttasim=${tce}/src/codesign/ttasim/ttasim
 tcecc=${tce}/src/bintools/Compiler/tcecc
+tce_ld=${tce}/src/ext/llvm-ld/tce-llvm-ld
 optlevel=-O0
 # this was default behavior
 scheduler_flags=-O3
@@ -11,5 +12,5 @@ verbose=-v
 
 cd DENBench
 #rm -fr DENBench/consumer/llvm-tce-systemtest 
-./run_systemtest.sh -c $tcecc -t $ttasim -a $machine $optlevel -s "$scheduler_flags" $verbose
+./run_systemtest.sh -c $tcecc -t $ttasim -l $tce_ld -a $machine $optlevel -s "$scheduler_flags" $verbose
 
