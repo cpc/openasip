fc7b41ff7e0ed0bc255ef1ce080ecf1755c81b25
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2014-01-13 19:51:54 +0200

    Revert the SVN checkout to ensure the patches apply if old patches have been applied.

--- tce/tools/scripts/install_llvm_3.4.sh
+++ tce/tools/scripts/install_llvm_3.4.sh
@@ -19,6 +19,7 @@ echo "### LLVM build mode: "$LLVM_BUILD_MODE
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 patch_dir=$script_dir/../patches
+llvm_co_dir=llvm-3.4svn
 
 temp_dir=llvm-build-temp
 mkdir -p $temp_dir
@@ -30,15 +31,17 @@ function eexit {
 }
 
 # Fetch LLVM from SVN.
-if ! test -d llvm-3.4svn;
+if ! test -d $llvm_co_dir;
 then
   svn -q co http://llvm.org/svn/llvm-project/llvm/branches/release_34 llvm-3.4svn \
   || eexit "SVN co from LLVM failed"
 else
-  svn up llvm-3.4svn || eexit "SVN update of LLVM failed."
+  svn up $llvm_co_dir || eexit "SVN update of LLVM failed."
+  # Wipe out possible previously applied patches.
+  svn revert -R $llvm_co_dir
 fi
 
-cd llvm-3.4svn/tools
+cd $llvm_co_dir/tools
 
 # Fetch Clang from SVN.
 if ! test -d clang;
@@ -46,13 +49,14 @@ then
 svn -q co http://llvm.org/svn/llvm-project/cfe/branches/release_34 clang \
   || eexit "SVN co from Clang failed" 
 else
-svn up clang || eexit "SVN update of Clang failed."
+  svn up clang || eexit "SVN update of Clang failed."
+  svn revert -R clang
 fi
 
-cd ../../llvm-3.4svn
+cd ../../$llvm_co_dir
 
-patch -Np0 < $patch_dir/clang-3.4-no-forced-64bit-doubles.patch
-patch -Np0 < $patch_dir/llvm-3.4-fix-SROA-creating-invalid-bitcasts-beween-address-spaces.patch
+patch -Np0 < $patch_dir/clang-3.4-no-forced-64bit-doubles.patch 
+patch -Np0 < $patch_dir/llvm-3.4-fix-SROA-creating-invalid-bitcasts-beween-address-spaces.patch 
 
 ./configure $LLVM_BUILD_MODE --enable-shared --prefix=$TARGET_DIR || eexit "Configuring LLVM/Clang failed."
 make -j2 REQUIRES_RTTI=1 || eexit "Building LLVM/Clang failed."
