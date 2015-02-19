cab58379eb0e7020326ae742a28362ecefaad550
Pekka Jääskeläinen <pekka.jaaskelainen@tut.fi>
2015-02-19 18:45:16 +0200

    Explicitly exit 0 as systemtest.py considers nonzero a failure from now on.

--- testsuite/systemtest/codesign/Explorer/ImmediateGenerator/run_3bus_not_fit.sh
+++ testsuite/systemtest/codesign/Explorer/ImmediateGenerator/run_3bus_not_fit.sh
@@ -7,3 +7,6 @@ WIDTH=99
 "${EXPLORE_BIN}" -a ./data/imm_test_3_bus.adf test3bne.dsdb &>/dev/null
 
 "${EXPLORE_BIN}" -e ImmediateGenerator -s 1 -u add_it_name="${TEMPLATE_NAME}" -u width=$WIDTH -u split='true' test3bne.dsdb 2>&1 | grep 99 | grep -v 3
+
+# The above explorer command is supposed to fail.
+exit 0
