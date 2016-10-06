656657a2130216917cf19857e40d22b1b435a8ee
noah the goodra <peterpan0413@live.com>
2016-10-06 00:08:20 -0500

    Update TravisScript.sh

--- TravisScript.sh
+++ TravisScript.sh
@@ -1,7 +1,7 @@
 cd tce
 ./tools/scripts/install_llvm_$LLVM_VER_BUILD.sh $INSTALLDIR/llvm
 export PATH="$HOME:$INSTALLDIR/llvm/bin:$PATH"
-if [ ! -f `llvm-config --libdir`/libLLVM-* ]; then ./gen_llvm_shared_lib.sh ;fi
+if [ ! -f '`llvm-config --libdir`/libLLVM-*' ]; then ./gen_llvm_shared_lib.sh ;fi
 ./autogen.sh
 ./configure --prefix=$INSTALLDIR
 make
