5284ce30bf90aee5dc253df308efc1630a12aed0
noah the goodra <peterpan0413@live.com>
2016-10-06 00:32:46 -0500

    added tcltk

--- TravisScript.sh
+++ TravisScript.sh
@@ -1,3 +1,18 @@
+#tcl tk install region
+wget --no-check-certificate http://prdownloads.sourceforge.net/tcl/tcl8.6.6-src.tar.gz
+tar -xzf tcl8.6.6-src.tar.gz
+cd tcl8.6.6/unix
+./configure --prefix=$INSTALLDIR/tcl
+make install
+export PATH="$INSTALLDIR/tcl/bin:$PATH"
+wget --no-check-certificate http://prdownloads.sourceforge.net/tcl/tk8.5.19-src.tar.gz
+tar -xzf tk8.5.19-src.tar.gz
+cd tk8.5.19/unix
+./configure --prefix=$INSTALLDIR/tk
+make install
+export PATH="$INSTALLDIR/tk/bin:$PATH"
+#tcl tk install region
+
 cd tce
 ./tools/scripts/install_llvm_$LLVM_VER_BUILD.sh $INSTALLDIR/llvm
 export PATH="$HOME:$INSTALLDIR/llvm/bin:$PATH"
