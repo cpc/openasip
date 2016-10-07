547d6e53219aca2198091adc73aee2b8ca4d1dac
noah the goodra <peterpan0413@live.com>
2016-10-07 12:50:38 -0500

    fixed the script syntax

--- FirstScript.sh
+++ FirstScript.sh
@@ -5,7 +5,7 @@ wget --no-check-certificate http://cmake.org/files/v3.4/cmake-3.4.3-Linux-x86_64
 tar -xzf cmake-3.4.3-Linux-x86_64.tar.gz
 export PATH=$PWD/cmake-3.4.3-Linux-x86_64/bin:$PATH
 rm cmake-3.4.3-Linux-x86_64.tar.gz
-;fi
+fi
 
 
 #- if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then wget --no-check-certificate http://llvm.org/releases/3.4.2/clang+llvm-3.4.2-x86_64-unknown-ubuntu12.04.xz ;fi
@@ -19,16 +19,17 @@ brew install ccache;
 brew install boost;
 brew install xerces-c;
 brew install llvm;
-export PATH=/usr/local/opt/llvm/bin:$PATH ;
+export PATH=/usr/local/opt/llvm/bin:$PATH
 fi
 
 if [ "$CC"  == "clang" ]; then 
 ln -s $(which ccache) $HOME/clang && ln -s $(which ccache) $HOME/clang++ && export PATH=$HOME:$PATH;
 export CC='clang -Qunused-arguments -fcolor-diagnostics'
 export CXX='clang++ -Qunused-arguments -fcolor-diagnostics' 
-;fi
-- if [ "$CC"  == "gcc" ]; then 
-export CC='gcc-4.8'; export CXX='g++'
-;fi
+fi
+
+if [ "$CC"  == "gcc" ]; then 
+export CC='gcc-4.8'; export CXX='g++-4.8'
+fi
 ccache -z -M 500M
 ./InstallScript.sh
