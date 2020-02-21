0bad7a33582fac5db9f29d6c53523f45189113f0
Heikki Kultala <heikki.kultala@tut.fi>
2020-02-21 16:28:50 +0200

    fix unit tests after the move smartpointer change.

--- tce/src/applibs/Simulator/POMGenMacros.hh
+++ tce/src/applibs/Simulator/POMGenMacros.hh
@@ -435,7 +435,7 @@ findBooleanGuard(const TTAMachine::Machine& machine, bool isInverted) {
     PRINTOUT(" "); \
     Bus* bus__ = &findBus(\
         machine__, std::string("bus") + Conversion::toString(moveCounter__));\
-    Move* tempMove = NULL;\
+    std::shared_ptr<Move> tempMove = NULL;                                              \
     CONSTRUCT_MOVE;\
     tempInstruction__->addMove(tempMove);\
 } moveCounter__++;
@@ -443,7 +443,7 @@ findBooleanGuard(const TTAMachine::Machine& machine, bool isInverted) {
 /**
  * Creates a basic move without any guards.
  */
-#define BASIC_MOVE_CONSTRUCTION tempMove = new Move(src__, dst__, *bus__)
+#define BASIC_MOVE_CONSTRUCTION tempMove = std::make_shared<Move>(src__, dst__, *bus__)
 
 /**
  * Creates a basic move and adds it to the instruction this macro is 
@@ -520,7 +520,7 @@ findBooleanGuard(const TTAMachine::Machine& machine, bool isInverted) {
 #define GUARDED_MOVE_CONSTRUCTION(ADFGUARD) \
     TTAMachine::Guard &adfGuard__ = ADFGUARD; \
     MoveGuard* guard__ = new MoveGuard(adfGuard__); \
-    tempMove = new Move(src__, dst__, *bus__, guard__);
+    tempMove = std::make_shared<Move>(src__, dst__, *bus__, guard__);
 
   
 /**
