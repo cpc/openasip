ab5f75ea523bb168ce6e3ed190aae8a1f30fdd39
Pekka Jääskeläinen <pekka.jaaskelainen@tuni.fi>
2020-11-07 09:33:34 +0200

    Avoid test hanging due to ttasim waiting for input if tcecc fails

--- testsuite/systemtest/bintools/Compiler/RegressionTests/test_bool_return_value.sh
+++ testsuite/systemtest/bintools/Compiler/RegressionTests/test_bool_return_value.sh
@@ -6,8 +6,7 @@ tpef=bool_return_value.tpef
 src=data/bool_return_value.cpp
 adf=../../../../../tce/data/mach/minimal.adf
 
-$tcecc -O0 --swfp -a $adf -o $tpef $src
-
+$tcecc -O0 --swfp -a $adf -o $tpef $src && \
 $ttasim -p $tpef -a $adf -e "run; puts [x /n 1 /u w eq]; puts [x /n 1 /u w gt]; puts [x /n 1 /u w nef]; puts [x /n 1 /u w gtf]; quit;"
 
 rm -f $tpef
