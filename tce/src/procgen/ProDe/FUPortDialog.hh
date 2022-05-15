d93fc9425ceabeba73d6e8c7ff97f7e41d7f3334
Sarah Clark <sarahclark@google.com>
2022-05-14 18:02:32 -0700

    Import missing <wx/wx.h>

--- tce/src/procgen/ProDe/FUPortDialog.hh
+++ tce/src/procgen/ProDe/FUPortDialog.hh
@@ -26,73 +26,75 @@
  *
  * Declaration of FUPortDialog class.
  *
- * @author Veli-Pekka Jääskeläinen 2004 (vjaaskel-no.spam-cs.tut.fi)
+ * @author Veli-Pekka Jï¿½ï¿½skelï¿½inen 2004 (vjaaskel-no.spam-cs.tut.fi)
  * @note rating: red
  */
 
 #ifndef TTA_FU_PORT_DIALOG_HH
 #define TTA_FU_PORT_DIALOG_HH
 
+#include <wx/wx.h>
+
 namespace TTAMachine {
-    class FUPort;
-    class Socket;
-}
+class FUPort;
+class Socket;
+} // namespace TTAMachine
 
 /**
  * Dialog for querying port parameters from the user.
  */
 class FUPortDialog : public wxDialog {
 public:
-    FUPortDialog(wxWindow* parent, TTAMachine::FUPort* port);
-    ~FUPortDialog();
+  FUPortDialog(wxWindow *parent, TTAMachine::FUPort *port);
+  ~FUPortDialog();
 
 protected:
-    virtual bool TransferDataToWindow();
+  virtual bool TransferDataToWindow();
 
 private:
-    wxSizer* createContents(wxWindow* parent, bool call_fit, bool set_sizer);
+  wxSizer *createContents(wxWindow *parent, bool call_fit, bool set_sizer);
 
-    void updateSockets();
-    void onOK(wxCommandEvent& event);
-    void onCancel(wxCommandEvent& event);
-    void onSocketChoice(wxCommandEvent& event);
-    void onHelp(wxCommandEvent& event);
-    void onName(wxCommandEvent& event);
-    void setTexts();
+  void updateSockets();
+  void onOK(wxCommandEvent &event);
+  void onCancel(wxCommandEvent &event);
+  void onSocketChoice(wxCommandEvent &event);
+  void onHelp(wxCommandEvent &event);
+  void onName(wxCommandEvent &event);
+  void setTexts();
 
-    /// Function unit port to modify.
-    TTAMachine::FUPort* port_;
-    /// Name of the port.
-    wxString name_;
-    /// Width of the port.
-    int width_;
-    /// Trigger attribute of the port.
-    bool triggers_;
-    /// Input socket choice control.
-    wxChoice* inputSocketChoice_;
-    /// Output socket choice control.
-    wxChoice* outputSocketChoice_;
+  /// Function unit port to modify.
+  TTAMachine::FUPort *port_;
+  /// Name of the port.
+  wxString name_;
+  /// Width of the port.
+  int width_;
+  /// Trigger attribute of the port.
+  bool triggers_;
+  /// Input socket choice control.
+  wxChoice *inputSocketChoice_;
+  /// Output socket choice control.
+  wxChoice *outputSocketChoice_;
 
-    /// Original input socket of the port.
-    TTAMachine::Socket* oldInput_;
-    /// Original output socket of the port.
-    TTAMachine::Socket* oldOutput_;
+  /// Original input socket of the port.
+  TTAMachine::Socket *oldInput_;
+  /// Original output socket of the port.
+  TTAMachine::Socket *oldOutput_;
 
-    enum {
-        ID_NAME=10000,
-        ID_WIDTH,
-        ID_INPUT_SOCKET,
-        ID_OUTPUT_SOCKET,
-        ID_TRIGGERS,
-        ID_HELP,
-        ID_LABEL_NAME,
-        ID_LABEL_WIDTH,
-        ID_LABEL_INPUT_SOCKET,
-        ID_LABEL_OUTPUT_SOCKET,
-        ID_LINE
-    };
+  enum {
+    ID_NAME = 10000,
+    ID_WIDTH,
+    ID_INPUT_SOCKET,
+    ID_OUTPUT_SOCKET,
+    ID_TRIGGERS,
+    ID_HELP,
+    ID_LABEL_NAME,
+    ID_LABEL_WIDTH,
+    ID_LABEL_INPUT_SOCKET,
+    ID_LABEL_OUTPUT_SOCKET,
+    ID_LINE
+  };
 
-    /// The event table for the controls of the dialog.
-    DECLARE_EVENT_TABLE()
+  /// The event table for the controls of the dialog.
+  DECLARE_EVENT_TABLE()
 };
 #endif
