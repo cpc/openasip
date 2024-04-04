deb33e66e41c427b9daae6c199170908d6c185eb
Joonas Multanen <joonas.multanen@tuni.fi>
2024-04-04 19:07:39 +0300

    Fix RFGen system test

--- testsuite/systemtest_long/procgen/RFGen/tcetest_rfgen.sh
+++ testsuite/systemtest_long/procgen/RFGen/tcetest_rfgen.sh
@@ -9,6 +9,7 @@ ENTITY="rfgen_core"
 TPEF="test.tpef"
 PROGE_OUT_BASIC="proge-out"
 PROGE_OUT_PARTIAL_IDF="proge-out-partial-idf"
+PROGE_OUT_VERILOG="proge-out-verilog"
 TTABUSTRACE=ttabustrace
 GOLDEN_RF_DUMP=data/rf.golden.dump
 ICD_OPTS="bustrace:yes,bustracestartingcycle:5"
@@ -45,6 +46,7 @@ abort-with-msg() {
 clear_test_data() {
     rm -rf $PROGE_OUT_BASIC
     rm -rf $PROGE_OUT_PARTIAL_IDF
+    rm -rf $PROGE_OUT_VERILOG
     rm -f $TPEF
     rm -f *.img
     rm -f *.tpef.*
@@ -56,7 +58,32 @@ function line-count() {
     wc -l < $1
 }
 
-function run_test() {
+function run_verilog_test() {
+    ADF=$1
+    EXTRA_OPTS=$2
+    PROGE_OUT=$3
+    $PROGE --icd-arg-list=$ICD_OPTS $EXTRA_OPTS -e $ENTITY -l "verilog" -t -o $PROGE_OUT $ADF \
+        || abort-with-msg "Error from ProGe"
+    $GENERATEBITS -e $ENTITY -x $PROGE_OUT -d -w4 -p $TPEF $ADF \
+        || abort-with-msg "Error from PIG"
+
+    # If iverilog is found from PATH, compile and simulate
+    IVERILOG=$(which iverilog 2> /dev/null)
+    if [ "x${GHDL}" != "x" ]
+    then
+        cd $PROGE_OUT || exit 1
+        ./iverilog_compile.sh >& /dev/null || echo "iverilog compile failed."
+        ./iverilog_simulate.sh >& /dev/null || echo "iverilog simulation failed."
+        cd ..
+        diff $TTABUSTRACE \
+             <(head -n $(wc -l < $TTABUSTRACE) < $PROGE_OUT/execbus.dump) \
+            || { echo "Verilog bustrace mismatch"; exit 1; }
+    fi
+
+    return 0
+}
+
+function run_vhdl_test() {
     ADF=$1
     EXTRA_OPTS=$2
     PROGE_OUT=$3
@@ -69,19 +96,23 @@ function run_test() {
     # Limit on real simulation time
     runtimelimit=10000
 
-    cd $PROGE_OUT
-    ./ghdl_compile.sh >& /dev/null || abort-with-msg "ghdl compile failed."
-    ./ghdl_simulate.sh -i $runexeclimit -r $runtimelimit \
-        >& /dev/null || abort-with-msg "ghdl simulation failed."
-    cd ..
-    diff $TTABUSTRACE \
-        <(head -n $(line-count $TTABUSTRACE) \
-        < $PROGE_OUT/execbus.dump) \
-        || abort-with-msg "Difference found in bustrace."
-    diff $GOLDEN_RF_DUMP \
-         $PROGE_OUT/rf.dump \
-        || abort-with-msg "Difference found in rf.dump."
-    
+    # If GHDL is found from PATH, compile and simulate
+    GHDL=$(which ghdl 2> /dev/null)
+    if [ "x${GHDL}" != "x" ]
+    then
+        cd $PROGE_OUT || exit 1
+        ./ghdl_compile.sh -v93c >& /dev/null || echo "ghdl compile failed."
+        ./ghdl_simulate.sh -v93c >& /dev/null || echo "ghdl simulation failed."
+        cd ..
+        diff $TTABUSTRACE \
+             <(head -n $(line-count $TTABUSTRACE) \
+                    < $PROGE_OUT/execbus.dump) \
+            || abort-with-msg "Difference found in bustrace."
+        diff $GOLDEN_RF_DUMP \
+             $PROGE_OUT/rf.dump \
+            || abort-with-msg "Difference found in rf.dump."
+    fi
+
     return 0
 }
 
@@ -89,14 +120,21 @@ clear_test_data
 
 tceasm -o $TPEF $ADF $SRC >& $ERRORLOG \
     || abort-with-msg "Error from tceasm."
+
 $GENBUSTRACE -l 200 -o $TTABUSTRACE $ADF $TPEF \
     || abort-with-msg "Error in bus trace generation."
 
-run_test "data/mach.adf" "" "$PROGE_OUT_BASIC"
-run_test "data/mach.adf" " -i data/mach.idf" "$PROGE_OUT_PARTIAL_IDF"
+#rm -rf $PROGE_OUT_VERILOG
+#run_test "data/mach.adf" "--hdl=verilog" "$PROGE_OUT_VERILOG" "verilog"
+#exit 0
+
+run_vhdl_test "data/mach.adf" "" "$PROGE_OUT_BASIC"
+run_vhdl_test "data/mach.adf" " -i data/mach.idf" "$PROGE_OUT_PARTIAL_IDF"
 if [[ ! -f "$PROGE_OUT_PARTIAL_IDF/vhdl/rf_1wr_1rd_always_1_guarded_0.vhd" ]]; then
     echo "IDF defined RF file not found"
 fi
+
+run_verilog_test "data/mach.adf" "" "$PROGE_OUT_VERILOG"
 
 [ "${leavedirty}" != "true" ] && clear_test_data
 exit 0
